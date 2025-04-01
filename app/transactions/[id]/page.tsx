774df6c678c2725e0dfe9ebfcd2bfd60d099f9c2
SEBASTIAO RODRIGO <rodrigoexer1@gmail.com>
2025-04-01 14:14:13 -0300

    updated-id-references-of-transactions

--- app/transactions/[id]/page.tsx
+++ app/transactions/[id]/page.tsx
@@ -1,15 +1,22 @@
-interface IdProps{
-    id: string
+import { NextPage } from "next";
+
+interface TransactionProps {
+  params: { id: string };
 }
-const Transaction = ({ params: { id }} ) => {
-    // http://localhost:3000/transactions/100
-    return(
-        <div className="flex justify-center items-center h-screen">
-            <h1 className="text-3xl">
-                Transaction id: 
-                <span className="text-4xl text-green-700 font-mono font-medium"> {id} </span>
-            </h1>
-        </div>
-    )
-}
-export default Transaction
+
+const Transaction: NextPage<TransactionProps> = ({ params }) => {
+  const { id } = params;
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl">
+        Transaction ID:{" "}
+        <span className="text-4xl text-green-700 font-mono font-medium">
+          {id}
+        </span>
+      </h1>
+    </div>
+  );
+};
+
+export default Transaction;
